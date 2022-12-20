191e85c05212968727f99dd10a9a749379b5ae4d
brunoamaia <brunoamaia94@gmail.com>
2022-12-20 20:50:44 -0300

    Instalação dos teste unitários

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
 	resolve: {
 		alias: [{ find: '@', replacement: '/src' }],
 	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['.test/setup.ts'],
+		include: ['**/*(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+	},
 })
